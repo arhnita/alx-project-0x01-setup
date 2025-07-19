4a3733e28cbf2400f6a271dcb3113cdf925141cc
Anita Samuel <migist2@gmail.com>
2025-07-19 06:38:57 +0100

    task 0

---
+++ alx-project-0x01/components/common/PostCard.tsx
@@ -0,0 +1,10 @@
+const PostCard: React.FC = () => {
+  return (
+    <div className="p-4 border rounded shadow">
+      <h3 className="text-lg font-semibold">Post Title</h3>
+      <p className="text-gray-600">Post content...</p>
+    </div>
+  );
+};
+
+export default PostCard;
