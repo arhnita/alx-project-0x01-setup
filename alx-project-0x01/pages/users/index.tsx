06e46f2bcb9ae254a7440e7083ff0292cb8800e9
Anita Samuel <migist2@gmail.com>
2025-07-19 15:05:12 +0100

    props

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,19 +1,36 @@
-const Users: React.FC = () => {
+import PostCard from "@/components/common/PostCard";
+import { PostProps } from "@/interfaces";
+
+interface PostPageProps {
+  posts: PostProps[];
+}
+const Users: React.FC<PostPageProps> = ({ posts }) => {
   return (
     <div>
       <h1 className="text-2xl font-bold">Users Page</h1>
+      <div>
+        {posts?.map(({ title, body, userId, id }: PostProps, key: number) => (
+          <PostCard
+            title={title}
+            body={body}
+            userId={userId}
+            id={id}
+            key={key}
+          />
+        ))}
+      </div>
     </div>
   );
 };
 
 export async function getStaticProps() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users")
-  const posts = await response.json()
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const posts = await response.json();
 
   return {
     props: {
-      posts
-    }
-  }
+      posts,
+    },
+  };
 }
 export default Users;
